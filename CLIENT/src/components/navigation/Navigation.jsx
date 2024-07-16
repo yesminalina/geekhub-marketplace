dee68b39faddaf0b41f42e2d51d19f0d8405083a
Alejandro Stuardo <alejandro.stuardoalarcon@gmail.com>
2024-07-15 22:38:19 -0400

    Add components

---
+++ CLIENT/src/components/navigation/Navigation.jsx
@@ -0,0 +1,49 @@
+import './Navigation.css'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import { NavLink } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <div>
+      <Navbar expand='lg' className='navbg'>
+        <Container fluid>
+          <Navbar.Brand><NavLink to='/'>GeekHub</NavLink></Navbar.Brand>
+          <Navbar.Toggle aria-controls='navbarScroll' />
+          <Navbar.Collapse id='navbarScroll'>
+            <Nav
+              className='me-auto my-2 my-lg-0'
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <NavLink to='/' className='navlinks'>Home</NavLink>
+              <NavDropdown title='Categorias' id='navbarScrollingDropdown'>
+                <NavDropdown.Item to='#action3' className='navdropitem'>Categoria 1</NavDropdown.Item>
+                <NavDropdown.Item to='#action4' className='navdropitem'>Categoria 2</NavDropdown.Item>
+                <NavDropdown.Item to='#action5' className='navdropitem'>Categoria 3</NavDropdown.Item>
+              </NavDropdown>
+              <NavLink to='/about-us' className='navlinks'>Sobre Nosotros</NavLink>
+              <NavLink to='/contact' className='navlinks'>Contacto</NavLink>
+              <NavLink to='/help' className='navlinks'>Ayuda</NavLink>
+            </Nav>
+            <Form className='d-flex'>
+              <Form.Control
+                type='search'
+                placeholder='Search'
+                className='me-2'
+                aria-label='Search'
+              />
+              <Button className='search-btn'>Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Navigation
