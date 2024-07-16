17ffdc0aebfb0c05900cbb28eee11e0c936967bc
Alejandro Stuardo <alejandro.stuardoalarcon@gmail.com>
2024-07-15 22:40:41 -0400

    Fix minor details

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import { Cart, Catalogue, Favorites, Home, Login, MyProducts, Payment, ProductDetails, Profile, Register, NotFound } from './views/indexViews.js'
+import { Cart, Catalogue, Favorites, Home, Login, MyProducts, Payment, ProductDetails, Profile, Register, NotFound, AboutUs, Help, Contact } from './views/indexViews.js'
+import Navigation from './components/navigation/Navigation.jsx'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Navigation />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/register' element={<Register />} />
@@ -16,7 +18,10 @@ const App = () => {
           <Route path='/cart' element={<Cart />} />
           <Route path='/payment' element={<Payment />} />
           <Route path='/catalogue' element={<Catalogue />} />
-          <Route path='/productdetails' element={<ProductDetails />} />
+          <Route path='/product-details' element={<ProductDetails />} />
+          <Route path='/about-us' element={<AboutUs />} />
+          <Route path='/help' element={<Help />} />
+          <Route path='/contact' element={<Contact />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
         <ToastContainer />
