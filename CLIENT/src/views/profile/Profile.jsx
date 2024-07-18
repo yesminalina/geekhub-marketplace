b37ff5f6e72616b9f32321c78565469062a7401b
yesminalina <yesmin.pizarro@gmail.com>
2024-07-17 23:12:39 -0400

    Add Profile view

--- CLIENT/src/views/profile/Profile.jsx
+++ CLIENT/src/views/profile/Profile.jsx
@@ -1,6 +1,20 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import ProfileMenu from '../../components/profileMenu/ProfileMenu'
+import ProfileForm from '../../components/profileForm/ProfileForm'
+import './Profile.css'
+
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <Container className='min-vh-100 py-5'>
+      <Row className='min-vh-100'>
+        <Col xs={4} className='border-end'>
+          <ProfileMenu />
+        </Col>
+        <Col xs={8}>
+          <ProfileForm />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 export default Profile
