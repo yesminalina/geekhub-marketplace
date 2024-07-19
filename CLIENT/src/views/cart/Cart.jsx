b4dd956796d506732de7830929ae12dc2389f349
yesminalina <yesmin.pizarro@gmail.com>
2024-07-18 23:49:29 -0400

    Add Cart view

--- CLIENT/src/views/cart/Cart.jsx
+++ CLIENT/src/views/cart/Cart.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import CartCard from '../../components/cartCard/CartCard'
 
 const cart = () => {
   return (
-    <div>cart</div>
+    <Container fluid className='min-vh-100 px-5 py-5'>
+      <h2 className='text-uppercase fs-4 mb-5'>Mi Carrito</h2>
+      <Container className='w-50 border border-2 rounded-2 px-4 pb-5'>
+        <Row className='justify-content-center gy-3'>
+          <Col xs={12}>
+            <CartCard />
+          </Col>
+          <Col xs={12}>
+            <CartCard />
+          </Col>
+          <Col xs={12}>
+            <CartCard />
+          </Col>
+          <Col xs={12} className='d-flex justify-content-between align-items-center px-5 py-2 fw-bold'>
+            <p>Subtotal</p>
+            <p className='text-end'>$3.000</p>
+          </Col>
+          <Col xs={12} className='text-center'>
+            <Button variant='primary' size='lg'>Proceder con la compra</Button>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
   )
 }
 
