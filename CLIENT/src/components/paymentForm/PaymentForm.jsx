76efe1814c8134c45f52dbc4ab2aed3502193257
yesminalina <yesmin.pizarro@gmail.com>
2024-07-19 00:31:15 -0400

    Add form component in Payment view

---
+++ CLIENT/src/components/paymentForm/PaymentForm.jsx
@@ -0,0 +1,36 @@
+import { Row, Col, Button, Form } from 'react-bootstrap'
+
+const PaymentForm = () => {
+  return (
+    <Form className='border border-2 rounded-2 px-4 py-5'>
+      <Row className='mb-3'>
+        <Col>
+          <Form.Label>Número de tarjeta</Form.Label>
+          <Form.Control placeholder='1111 2222 333 444' />
+        </Col>
+        <Col>
+          <Form.Label>CVC</Form.Label>
+          <Form.Control placeholder='555' />
+        </Col>
+      </Row>
+      <Form.Group className='mb-3'>
+        <Form.Label>Nombre del titular</Form.Label>
+        <Form.Control placeholder='Dominga Chuky' />
+      </Form.Group>
+      <Form.Label>Fecha de vencimiento</Form.Label>
+      <Row className='mb-3'>
+        <Col xs={2}>
+          <Form.Control placeholder="15" />
+        </Col>
+        <Col xs={6}>
+          <Form.Control placeholder="Noviembre" />
+        </Col>
+        <Col xs={4}>
+          <Form.Control placeholder="2027" />
+        </Col>
+      </Row>
+      <Button variant='primary' type='submit' size='lg' className='w-100 mt-2'>Pagar</Button>
+    </Form>
+  )
+}
+export default PaymentForm
