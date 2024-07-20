c668856c5b0ab8f44bc5dff5dd96cf64a915e9cc
Eduardo Molina <eduardo.molina.me@hotmail.com>
2024-07-19 22:32:07 -0400

    Create and add folder context with ProductContex.jsx

---
+++ CLIENT/src/context/ProductsContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useEffect, useState } from 'react'
+export const ProductContext = createContext()
+
+const ProductsContextProvider = ({ children }) => {
+  const [products, setProducts] = useState([])
+  const liked = products.filter((product) => product.liked)
+  const getProducts = async () => {
+    const res = await fetch('./MOCK_DATA.json')
+    const data = await res.json()
+    setProducts(data)
+  }
+  useEffect(() => { getProducts() }, [])
+  const toggleLike = (id) => {
+    const newProducts = products.map((product) => {
+      if (product.id === id) {
+        return ({ ...product, liked: !product.liked })
+      }
+      return product
+    })
+    setProducts(newProducts)
+  }
+  const globalState = { products, liked, toggleLike }
+  return (
+    <ProductContext.Provider value={globalState}>
+      {children}
+    </ProductContext.Provider>
+  )
+}
+
+export default ProductsContextProvider
