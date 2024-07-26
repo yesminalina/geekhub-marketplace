f414b86dbffde7930cf1df433887a35bc9e6eef6
Alejandro Stuardo <alejandro.stuardoalarcon@gmail.com>
2024-07-26 02:24:51 -0400

    Add funtion

--- CLIENT/src/views/cart/Cart.jsx
+++ CLIENT/src/views/cart/Cart.jsx
@@ -18,20 +18,24 @@ const Cart = () => {
     <Container fluid className='px-5 py-5'>
       <Container className='w-75 px-4 pb-5 cart-container'>
         <h2 className='mt-4 mb-3 text-center title-name'>Mi Carrito</h2>
-        <Row className='justify-content-center gy-3'>
-          {cart.map((product) => (
-            <Col key={product.id} xs={12}>
-              <CartCard product={product} />
+        {cart.length > 0
+          ? <Row className='justify-content-center gy-3'>
+            {cart.map((product) => (
+              <Col key={product.id} xs={12}>
+                <CartCard product={product} />
+              </Col>
+            ))}
+            <Col xs={12} className='d-flex justify-content-between align-items-center px-5 py-2 fw-bold'>
+              <p>Subtotal</p>
+              <p className='text-end'>${total}</p>
             </Col>
-          ))}
-          <Col xs={12} className='d-flex justify-content-between align-items-center px-5 py-2 fw-bold'>
-            <p>Subtotal</p>
-            <p className='text-end'>${total}</p>
-          </Col>
-          <Col xs={12} className='text-center'>
-            <Button variant='primary' size='lg' onClick={() => navigate('/payment')}>Proceder con la compra</Button>
-          </Col>
-        </Row>
+            <Col xs={12} className='text-center'>
+              <Button variant='primary' size='lg' onClick={() => navigate('/payment')}>Proceder con la compra</Button>
+            </Col>
+          </Row>
+          : <Container>
+            <h3 className='text-center'>Tu carrito está vacío</h3>
+          </Container>}
       </Container>
     </Container>
   )
