2bb4ad7847a8d132a405e7419b2b13d8d9a6f976
yesminalina <yesmin.pizarro@gmail.com>
2024-07-25 20:34:08 -0400

    Fix cart section

--- CLIENT/src/views/payment/Payment.jsx
+++ CLIENT/src/views/payment/Payment.jsx
@@ -1,8 +1,16 @@
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import CartCard from '../../components/cartCard/CartCard'
 import PaymentForm from '../../components/paymentForm/PaymentForm'
+import { CartContext } from '../../context/CartContext'
+import { useContext, useEffect } from 'react'
 
 const Payment = () => {
+  const { cart, total, getTotal } = useContext(CartContext)
+
+  useEffect(() => {
+    getTotal()
+  }, [cart])
+
   return (
     <Container fluid className='min-vh-100 px-5 py-5'>
       <Row>
@@ -10,18 +18,17 @@ const Payment = () => {
           <Container className='cart-container py-3'>
             <h2 className='mb-3 title-name'>Revisa tu compra y paga</h2>
             <Row className='justify-content-center gy-3'>
-              <Col xs={12}>
-                <CartCard />
-              </Col>
-              <Col xs={12}>
-                <CartCard />
-              </Col>
-              <Col xs={12}>
-                <CartCard />
-              </Col>
+              {cart.map((product) => (
+                <Col key={product.id} xs={12}>
+                  <CartCard product={product} />
+                </Col>
+              ))}
               <Col xs={12} className='d-flex justify-content-between align-items-center px-5 py-2 fw-bold'>
-                <p className='fs-4'>Total</p>
-                <p className='fs-4 text-end'>$3.000</p>
+                <p>Subtotal</p>
+                <p className='text-end'>${total}</p>
+              </Col>
+              <Col xs={12} className='text-center'>
+                <Button variant='primary' size='lg'>Proceder con la compra</Button>
               </Col>
             </Row>
           </Container>
