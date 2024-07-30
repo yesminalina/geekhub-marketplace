ff6d05bf18506ed3e86773eb5a119fcd820009e0
Alejandro Stuardo <alejandro.stuardoalarcon@gmail.com>
2024-07-30 00:09:21 -0400

    Add Links

--- CLIENT/src/components/navigation/Navigation.jsx
+++ CLIENT/src/components/navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import './Navigation.css'
 import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import { NavLink, useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import logo from '../../assets/img/logo-pink.png'
@@ -70,11 +70,11 @@ const Navigation = () => {
               <Nav className='linksContainer me-auto my-2 my-lg-0' style={{ maxHeight: '100px' }} navbarScroll>
                 <NavLink to='/' className='navlinks'>Inicio</NavLink>
                 <NavDropdown title='Categorias' id='navbarScrollingDropdown'>
-                  <NavDropdown.Item to='#action3' className='navdropitem' name='Juegos de Mesa' onClick={handleFilterLink}>Juegos de Mesa</NavDropdown.Item>
-                  <NavDropdown.Item to='#action4' className='navdropitem' name='TCG' onClick={handleFilterLink}>TCG</NavDropdown.Item>
-                  <NavDropdown.Item to='#action5' className='navdropitem' name='Figuras Coleccionables' onClick={handleFilterLink}>Figuras Coleccionables</NavDropdown.Item>
-                  <NavDropdown.Item to='#action5' className='navdropitem' name='Mangas y Cómics' onClick={handleFilterLink}>Mangas y Cómics</NavDropdown.Item>
-                  <NavDropdown.Item to='#action5' className='navdropitem' name='Álbumes y Láminas' onClick={handleFilterLink}>Álbumes y Láminas</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='boardgames' className='navdropitem' name='Juegos de Mesa' onClick={handleFilterLink}>Juegos de Mesa</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='tcg' className='navdropitem' name='TCG' onClick={handleFilterLink}>TCG</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='figures&collectionable' className='navdropitem' name='Figuras Coleccionables' onClick={handleFilterLink}>Figuras Coleccionables</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='mangas&comics' className='navdropitem' name='Mangas y Cómics' onClick={handleFilterLink}>Mangas y Cómics</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='albums&laminas' className='navdropitem' name='Álbumes y Láminas' onClick={handleFilterLink}>Álbumes y Láminas</NavDropdown.Item>
                 </NavDropdown>
                 <NavLink to='/about-us' className='navlinks'>Sobre Nosotros</NavLink>
                 <NavLink to='/contact' className='navlinks'>Contacto</NavLink>
