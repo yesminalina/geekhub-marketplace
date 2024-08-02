7909323d52c9ebb463d7d84492ffd75921c25757
Alejandro Stuardo <alejandro.stuardoalarcon@gmail.com>
2024-08-02 02:37:36 -0400

    Fix style and funtionality

--- CLIENT/src/components/Liked/Liked.jsx
+++ CLIENT/src/components/Liked/Liked.jsx
@@ -1,44 +1,43 @@
 import './Liked.css'
 import { useContext } from 'react'
 import { ProductsContext } from '../../context/ProductsContext'
+import { CartContext } from '../../context/CartContext'
 import { Button, Col, Container, Row } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import IconHeart from '../iconHeart/IconHeart'
+import { useNavigate } from 'react-router-dom'
 
-const cart = <FontAwesomeIcon icon={faShoppingCart} size='2x' />
+const cart = <FontAwesomeIcon icon={faShoppingCart} size='3x' />
 
 const Liked = () => {
   const { liked, toggleLike } = useContext(ProductsContext)
+  const { addToCart } = useContext(CartContext)
+  const navigate = useNavigate()
+
   return (
     <Container className='pt-5'>
-      <h3>Mis Favoritos</h3>
+      <h1>Mis Favoritos</h1>
       <div className='d-flex justify-content-center'>
-        <div className='m-3 border p-4'>
+        <div className='likeContainer m-3 border p-4'>
           {liked.map((like) => (
             <div key={like.id}>
               <div className='d-flex justify-content-between w-100'>
                 <div className='d-flex gap-2 justify-content-start align-items-center'>
-                  <img src={like.image_url} alt='' style={{ width: '180px', cursor: 'pointer', paddingLeft: '20px' }} /* onClick={() => productoDetails(like.id)} */ />
+                  <img src={like.image_url} alt='' style={{ width: '180px', cursor: 'pointer', paddingLeft: '20px' }} onClick={() => navigate(`/product-details/${like.id}`)} />
                   <div className='containerLike'>
                     <h5 className='m-0 text-capitalize'>{like.title}</h5>
                     <h6 className='text-dark'>Precio:{like.price}</h6>
                     <h6 className='text-dark'>Descripción:{like.description}</h6>
                   </div>
                 </div>
-                <Col md={6} className='d-flex justify-content-end align-items-center'>
+                <Col md={6} className='d-flex justify-content-center align-items-center'>
                   <div className='text-start'>
                     <Row>
                       <Col>
-                        <h5 className='text-dark'>Aqui van los iconos</h5>
-                      </Col>
-                      <Col>
-                        <h5 className='text-success'>{like.price}</h5>
-                      </Col>
-                      <Col>
                         <section style={{ paddingRight: '20px' }} className='d-flex align-items-center justify-content-end'>
                           <Button onClick={() => { toggleLike(like.id) }}>{like.liked ? <IconHeart filled /> : <IconHeart />}</Button>
-                          <Button>{cart}</Button>
+                          <Button onClick={() => addToCart(like)}>{cart}</Button>
                         </section>
                       </Col>
                     </Row>
@@ -48,8 +47,6 @@ const Liked = () => {
               <hr />
             </div>
           ))}
-          {/* <h3>Total: ${totalPizzas}</h3>
-          <Button className='btn btn-success' onClick={() => handlePagar(totalPizzas)}>Ir a Pagar</Button> */}
         </div>
       </div>
     </Container>
--- CLIENT/src/views/favorites/Favorites.jsx
+++ CLIENT/src/views/favorites/Favorites.jsx
@@ -6,12 +6,9 @@ import Liked from '../../components/Liked/Liked'
 const Favorites = () => {
   const { liked } = useContext(ProductsContext)
   return (
-    <>
-      <div className='Fav'>
-        <h1>Productos favoritos</h1>
-        {liked.length ? <Liked /> : <div><h3>Agrega tus productos favoritos</h3><img src='https://i.pinimg.com/originals/8f/6a/2b/8f6a2bf112d267932b31004311d8fc85.gif' alt='sin favoritos' /></div>}
-      </div>
-    </>
+    <div className='fav'>
+      {liked.length ? <Liked /> : <div><h3>Agrega tus productos favoritos de la tienda</h3></div>}
+    </div>
   )
 }
 export default Favorites
