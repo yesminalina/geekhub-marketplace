3afce40569997a1e393a71e64001dc66dfe80f05
Alejandro Stuardo <alejandro.stuardoalarcon@gmail.com>
2024-08-02 21:41:12 -0400

    Add notFound funtion

--- CLIENT/src/views/notFound/NotFound.jsx
+++ CLIENT/src/views/notFound/NotFound.jsx
@@ -1,6 +1,24 @@
+import './NotFound.css'
+import back from '../../assets/img/404.png'
+import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+
 const NotFound = () => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/')
+  }
   return (
-    <div>NotFound</div>
+    <div className='notFound'>
+      <img src={back} className='img404' />
+      <div className='text-overlay'>
+        <h1>Lo sentimos!</h1>
+        <h1>Estás perdido en el espacio</h1>
+        <p>Parece que la página que estás intentando visitar se ha movido o ya no existe. Por favor verifique la dirección para asegurarse de haber escrito correctamente o regrese a la página principal.</p>
+        <Button id='goHome' onClick={handleClick}>Volver al Inicio</Button>
+      </div>
+    </div>
   )
 }
 export default NotFound
