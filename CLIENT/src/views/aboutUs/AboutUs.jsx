584579d9c161fb21d7917e976bb75b805b37b1da
Alejandro Stuardo <alejandro.stuardoalarcon@gmail.com>
2024-08-05 02:12:29 -0400

    Add about us

--- CLIENT/src/views/aboutUs/AboutUs.jsx
+++ CLIENT/src/views/aboutUs/AboutUs.jsx
@@ -1,7 +1,49 @@
-const SobreNosotros = () => {
+import './AboutUs.css'
+import { Card } from 'react-bootstrap'
+
+const AboutUs = () => {
   return (
-    <div>SobreNosotros</div>
+    <Card className='aboutUs'>
+      <Card.Title className='titleUs'>Sobre Nosotros</Card.Title>
+      <h4>
+        Bienvenidos a GeekHub, tu paraíso de coleccionables y artículos geek!
+      </h4>
+      <Card.Text>
+        En GeekHub, nos apasiona todo lo relacionado con el mundo geek y los coleccionables. Nuestra tienda nació del amor por la cultura pop, los cómics, las figuras de acción, los videojuegos y todas esas cosas maravillosas que hacen que nuestro corazón friki lata más rápido. Desde nuestro lanzamiento, hemos estado dedicados a proporcionar a nuestros clientes los mejores productos y una experiencia de compra inigualable.
+      </Card.Text>
+      <h4>
+        Nuestra Misión
+      </h4>
+      <Card.Text>
+        Nuestra misión es ser el destino número uno para todos los entusiastas de los coleccionables y la cultura geek. Nos esforzamos por ofrecer una amplia variedad de productos de alta calidad, desde las figuras más recientes hasta los artículos más raros y difíciles de encontrar. Queremos que cada visita a GeekHub sea una aventura emocionante, donde puedas descubrir ese objeto especial que estabas buscando.
+      </Card.Text>
+      <h4>
+        Qué Ofrecemos
+      </h4>
+      <Card.Text>
+        En GeekHub, encontrarás:
+        Figuras de Acción: Desde tus héroes favoritos de cómics hasta personajes icónicos de películas y series de televisión.
+        Estatuillas y Réplicas: Detalladas y de alta calidad, perfectas para cualquier coleccionista.
+        Juegos y Puzzles: Para todas las edades y niveles de habilidad.
+        Ropa y Accesorios: Muestra tu amor por tus franquicias favoritas con estilo.
+        Artículos de Edición Limitada: Para aquellos coleccionistas que buscan algo verdaderamente especial.
+      </Card.Text>
+      <h4>
+        Nuestra Comunidad
+      </h4>
+      <Card.Text>
+        Más que una tienda, GeekHub es una comunidad. Nos encanta conectarnos con otros aficionados y compartir nuestra pasión. Organizamos eventos, lanzamientos exclusivos y más, para que puedas disfrutar y compartir tu amor por todo lo geek.
+      </Card.Text>
+      <h4>
+        Compromiso con la Calidad
+      </h4>
+      <Card.Text>
+        En GeekHub, nos comprometemos a ofrecer productos de la más alta calidad. Trabajamos directamente con proveedores y fabricantes de renombre para asegurarnos de que cada artículo que encuentres en nuestra tienda cumpla con tus expectativas.
+        Gracias por elegir GeekHub como tu tienda de confianza para todos tus artículos coleccionables y geek. Esperamos que disfrutes de tu experiencia con nosotros y encuentres todo lo que buscas y más. ¡Explora, colecciona y disfruta!
+      </Card.Text>
+
+    </Card>
   )
 }
 
-export default SobreNosotros
+export default AboutUs
