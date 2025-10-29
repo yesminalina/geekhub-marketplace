e60cc54a6a15eb39720dfe3b942b0c1b3b11fcff
yesmin pizarro <yesmin.pizarro@gmail.com>
2025-10-29 01:32:54 -0300

    feat(data): add category list with imported images

---
+++ CLIENT/src/assets/data/categories.js
@@ -0,0 +1,30 @@
+import catBoard from '../../assets/img/cat-board.jpg'
+import catTCG from '../../assets/img/cat-tcg.jpg'
+import catFigures from '../../assets/img/cat-figures.jpg'
+import catComics from '../../assets/img/cat-comics.jpg'
+import catAlbums from '../../assets/img/cat-albums.jpg'
+
+const categories = [
+  {
+    name: 'Juegos de Mesa',
+    img: catBoard
+  },
+  {
+    name: 'TCG',
+    img: catTCG
+  },
+  {
+    name: 'Figuras Coleccionables',
+    img: catFigures
+  },
+  {
+    name: 'Mangas y Cómics',
+    img: catComics
+  },
+  {
+    name: 'Álbumes y Láminas',
+    img: catAlbums
+  }
+]
+
+export default categories
